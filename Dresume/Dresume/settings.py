c3921b7c4f7ff15fadd69244ac4b7aca1edc1717
Your Name <youremail@example.com>
2024-08-26 02:04:27 -0400

    update commit

--- Dresume/Dresume/settings.py
+++ Dresume/Dresume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$-^-6)01ho7+(31+z%5qf4e@fcskf68sg2fpp^hr*3+tm&!*0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app',]
 
 
 # Application definition
