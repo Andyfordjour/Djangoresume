33b7c77f6999edc37bffe9c99331de2a863b93ac
Your Name <youremail@example.com>
2024-08-26 02:24:43 -0400

    update commit

--- Dresume/Dresume/settings.py
+++ Dresume/Dresume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$-^-6)01ho7+(31+z%5qf4e@fcskf68sg2fpp^hr*3+tm&!*0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -73,14 +73,14 @@ WSGI_APPLICATION = 'Dresume.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
